93d0759a77427cbb7c9c41616e38bcbd4c84e489
camiodvl <camilo.228@hotmail.com>
2023-09-26 22:11:37 -0500

    FEAT: Add and Get

--- models/persona.js
+++ models/persona.js
@@ -1,2 +1,16 @@
 import mongoose from "mongoose";
 
+const personaSchema = mongoose.Schema({
+    name: { type: String, 
+    },
+    apellido : {
+        type: String
+    },
+    edad: {
+        type: Number
+    }
+});
+
+const Persona = mongoose.model('Persona', personaSchema)
+
+export default Persona;
--- server.js
+++ server.js
@@ -1,15 +1,40 @@
 import express from "express";
 import mongoose from "mongoose";
+import Persona from "./models/persona.js";
 
 //se crea el servidor
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({
-    nombre: "Todos los archivos",
-  });
+//middleware
+app.use(express.json())
+
+//ruta get /
+app.get("/", async(req, res) => {
+    res.json(await Persona.find())
+
 });
 
+
+//ruta post
+app.post("/", async(req, res)=>{
+  try{
+    const persona = await Persona.create(req.body);
+    res.json({
+      "Estado": "OK"
+    })
+    
+  }catch (error){
+    console.log(error.message)
+  }
+
+} )
+
+app.put("/:id", async(req, res)=>{
+  
+
+})
+
+//conexión a la bd
 mongoose.connect(
   "mongodb://userbd:otXhM4RolaOxGcBG@cluster0-shard-00-00.hgkh4.mongodb.net:27017,cluster0-shard-00-01.hgkh4.mongodb.net:27017,cluster0-shard-00-02.hgkh4.mongodb.net:27017/arquitectura?ssl=true&replicaSet=atlas-mg7654-shard-0&authSource=admin&retryWrites=true&w=majority"
 ).then(()=>{
